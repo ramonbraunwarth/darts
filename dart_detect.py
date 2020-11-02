d8d52bb5aa1dc04287f9f9ffdd553435f2e0fcf2
Max Reinhardt <Max@Maxs-MacBook-Pro-2.local>
2020-11-02 21:15:23 +0100

    first lines of code

---
+++ dart_detect.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# In[2]:
+
+
+cap = cv2.VideoCapture(0)
+
+# get width
+width = cap.get(3)
+print('width: ' + str(width))
+# get height
+height = cap.get(4)
+print('height: ' + str(height))
+
+#set wanted width and heigth
+#ret = cap.set(3, width/4)
+#ret = cap.set(4, height/4)
+
+# check width
+width = cap.get(3)
+print('new width: ' + str(width))
+# check height
+height = cap.get(4)
+print('new height: ' + str(height))
+
+
+# In[3]:
+
+
+# show video (in extra window), stop by pressing q
+#cv2.startWindowThread()      # else window won't close when q is pressed
+while(True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+
+    # Our operations on the frame come here
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Display the resulting frame
+    cv2.imshow('frame',gray)
+    key = cv2.waitKey(0) & 0xFF
+#    if key == ord("q"):
+#        break
+    if cv2.waitKey(0) == ord('q'):
+        break
+        
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
+
+
+# In[11]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
